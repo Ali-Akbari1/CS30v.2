e92c487ee8b765764a9d219d651793c65f87607a
zwarr <zwarr@10.0.0.29>
2022-04-12 19:29:04 -0600

    Finished FindName and Added Screen Dump

--- Chapter9/src/FindName/FindName.java
+++ Chapter9/src/FindName/FindName.java
@@ -11,7 +11,7 @@ public class FindName
 	String FindNam;
 	
 	Scanner input = new Scanner(System.in);
-	System.out.println("How many names would you like to fill the array with");
+	System.out.println("How Many Names Would You Like To Fill The Array With");
 	int userInput = input.nextInt();
 	
 	
@@ -19,14 +19,13 @@ public class FindName
 	
 	for (int i = 0; i < userInput; i++)
 	{
-		System.out.println("Type in the name you would like to fill the array with");
+		System.out.println("Type in the Name you Would Like to Fill the Array With");
 		NameArray[i] = input.next();
 	}
 	
 	
-	System.out.println("Enter the name you would like to search for");
+	System.out.println("Enter the Name you Would Like to Search for");
 	FindNam = input.next();
-	
 	
 	location = Search.linear(NameArray, FindNam);
 	if (location == -1)
@@ -36,8 +35,77 @@ public class FindName
 	
 	else 
 	{
-		System.out.println("First Occurence is element " + location);
+		System.out.println("First Occurence is Element " + location);
 	}
 	
 	}
 }
+
+
+
+
+/* SCREEN DUMP
+
+
+
+
+
+How Many Names Would You Like To Fill The Array With
+3
+Type in the Name you Would Like to Fill the Array With
+Ali
+Type in the Name you Would Like to Fill the Array With
+Carter
+Type in the Name you Would Like to Fill the Array With
+Surkhab
+Enter the Name you Would Like to Search for
+Ali
+First Occurence is Element 0
+
+
+
+
+
+
+
+
+How Many Names Would You Like To Fill The Array With
+3
+Type in the Name you Would Like to Fill the Array With
+Ali
+Type in the Name you Would Like to Fill the Array With
+Carter
+Type in the Name you Would Like to Fill the Array With
+Surkhab
+Enter the Name you Would Like to Search for
+Carter
+First Occurence is Element 1
+
+
+
+
+
+
+
+
+How Many Names Would You Like To Fill The Array With
+3
+Type in the Name you Would Like to Fill the Array With
+Ali
+Type in the Name you Would Like to Fill the Array With
+Carter
+Type in the Name you Would Like to Fill the Array With
+Surkhab
+Enter the Name you Would Like to Search for
+Surkhab
+First Occurence is Element 2
+
+
+
+
+
+
+
+
+
+*/
--- Chapter9/src/FindName/Search.java
+++ Chapter9/src/FindName/Search.java
@@ -2,25 +2,29 @@ package FindName;
 
 
 
-public class Search {
-
-
-	
+public class Search{	
 	public static int linear(String[] array, String NamToFind) {
 		int index = 0;
 		int size = array.length;
-		
-		while ((array[index] != NamToFind) && (index < size - 1)) {
+
+		while ((!array[index].equals(NamToFind)) && (index < size - 1)) 
+		{
 			index += 1;
 		}
 		
-		if (array[index] == NamToFind) 
+
+		
+		if (array[index].equals(NamToFind)) 
 		{
+
 			return(index);
+			
 		} 
 		else 
 		{
+			
 			return(-1);
+			
 		}
 	}
 }
