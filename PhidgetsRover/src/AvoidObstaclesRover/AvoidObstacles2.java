c7dceb2a33ffcad98833cfac714a78290aa5a091
904086002 <904086002@L815WD220002.edu.cbe.ab.ca>
2022-05-18 11:49:31 -0600

    Started Rover Challenge may 18

---
+++ PhidgetsRover/src/AvoidObstaclesRover/AvoidObstacles2.java
@@ -0,0 +1,61 @@
+package AvoidObstaclesRover;
+
+	//Add Phidgets Library
+	import com.phidget22.*;
+
+	public class AvoidObstacles2 {
+	    public static void main(String[] args) throws Exception {
+
+	        //Connect to wireless rover
+	        Net.addServer("", "192.168.100.1", 5661, "", 0);
+
+	        //Create
+	        DCMotor leftMotors = new DCMotor();
+	        DCMotor rightMotors = new DCMotor();
+	        DistanceSensor sonar = new DistanceSensor();
+
+	        //Address
+	        leftMotors.setChannel(0);
+	        rightMotors.setChannel(1);
+
+	        //Open
+	        leftMotors.open(5000);
+	        rightMotors.open(5000);
+	        sonar.open(5000);
+
+	        while (true) {
+
+	            System.out.println("Distance: " + sonar.getDistance() + " mm");
+	            
+	            if (sonar.getDistance() < 450) {
+	                //Object detected! Stop motors
+	                leftMotors.setTargetVelocity(-1);
+	                rightMotors.setTargetVelocity(-1);
+	    	        //Wait for 1 second
+	    	        Thread.sleep(2000);
+	    	        
+	    	        
+	    	        //Turn in one direction
+	    	        leftMotors.setTargetVelocity(-1);
+	    	        rightMotors.setTargetVelocity(1);
+
+	    	        //Wait for 2 second
+	    	        Thread.sleep(1000);
+
+	    	        //Move forward at full speed
+	    	        leftMotors.setTargetVelocity(-1);
+	    	        rightMotors.setTargetVelocity(-1);
+	    	        
+	    	        Thread.sleep(2000);
+	            } else {
+	                //Move forward slowly (25% max speed)
+	                leftMotors.setTargetVelocity(0.5);
+	                rightMotors.setTargetVelocity(0.5);
+	            }
+
+	            //Wait for 250milliseconds
+	            Thread.sleep(250);
+	        }
+	    }
+	}
+	  
