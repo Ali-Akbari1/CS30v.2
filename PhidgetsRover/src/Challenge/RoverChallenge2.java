9c0b3466805d6bccb7e22c396b042894980b7b5d
904086002 <904086002@L815WD220002.edu.cbe.ab.ca>
2022-06-06 12:01:19 -0600

    almost done rover challenge

--- PhidgetsRover/src/Challenge/RoverChallenge2.java
+++ PhidgetsRover/src/Challenge/RoverChallenge2.java
@@ -6,35 +6,8 @@ import com.phidget22.Net;
 
 public class RoverChallenge2 
 {
-	
-	
-	
-	
-	
-	
-	
-	
-	 
-    public static boolean checkSonar(DistanceSensor sonar, int time) throws Exception // Checks sonar while rover is moving, boolean used to stop rover
-	{
-		boolean hit = false;
-		
-		for (int i = time / 5; i != 0; i--)
-		{
-			if (sonar.getDistance() < 500)
-			{
-				hit = true;
-				break;
-			}
-			else
-			{
-				Thread.sleep(5);
-			}
-		}
-		return hit;
-	}
-    
-	
+
+
 	
 	public static void main(String[] args) throws Exception {
 
@@ -55,52 +28,18 @@ public class RoverChallenge2
         rightMotors.open(5000);
         sonar.open(5000);
         sonar.setDataInterval(100);
+        int count = 0;
         
         
-        
-        
-        
-        
-        
-        for (int i = 4; i != 0; i--)
-        {
-        	//Go forwards
-            leftMotors.setTargetVelocity(-1);
-            rightMotors.setTargetVelocity(-1);
-
-            if (checkSonar(sonar, 800) == true) //Continue unless/until checkSonar returns true
-            {									//Times to turn 90 and 180 degrees are basically approximated from trial and error and usually end up a bit wrong
-            	//Stop
-            	leftMotors.setTargetVelocity(0);
-                rightMotors.setTargetVelocity(0);
-            	Thread.sleep(1000);
-            	
-            	//Turn roughly 180 degrees
-            	leftMotors.setTargetVelocity(-1);
-                rightMotors.setTargetVelocity(1);
-                Thread.sleep(800);
-            }
-
-            //Stop motors
-            leftMotors.setTargetVelocity(0);
-            rightMotors.setTargetVelocity(0);
-            
-            Thread.sleep(1000);
-        
-        
-        
-        
-        
-        
-        
-        
+    
         
 
         while (true) {
 
             System.out.println("Distance: " + sonar.getDistance() + " mm");
+            count++;
             
-            if (sonar.getDistance() < 200) {
+            if (sonar.getDistance() < 450) {
             	
                 //Object detected! Stop motors
                 leftMotors.setTargetVelocity(0);
@@ -119,133 +58,51 @@ public class RoverChallenge2
     	        Thread.sleep(1000);
 
     	        //Move forward at full speed
-    	        leftMotors.setTargetVelocity(1);
-    	        rightMotors.setTargetVelocity(1);
+    	        leftMotors.setTargetVelocity(0);
+    	        rightMotors.setTargetVelocity(0);
     	        
     	        Thread.sleep(2000);
     	        
+    	        count = 10 - count;
+    	        
             }
             
-            
+      
             
             else 
+            	
             {
+            	
+            	if (count < 10){
             	
             	//Move forward at full speed
                 leftMotors.setTargetVelocity(1);
                 rightMotors.setTargetVelocity(1);
+                
+            	}
+            	
+            	
+            	else 
+            	{
+                	//90 degree turn
+                	leftMotors.setTargetVelocity(0);
+                	rightMotors.setTargetVelocity(0);
+               	 	Thread.sleep(2000);
+               	 	
+                    //Turn in one direction
+                    leftMotors.setTargetVelocity(-1);
+                    rightMotors.setTargetVelocity(1);
 
-                //Wait for 1 second
-                Thread.sleep(1750);
+                    //Wait for 2 second
+                    Thread.sleep(620);
+                    
+                	leftMotors.setTargetVelocity(0);
+                	rightMotors.setTargetVelocity(0);
+               	 	Thread.sleep(2000);
+                    
+                    count = 0;
+            	}
                 
-                //Stop motors
-                leftMotors.setTargetVelocity(0);
-                rightMotors.setTargetVelocity(0);
-                
-                //Wait for 2 second
-                Thread.sleep(2000);
-                
-                //Turn in one direction
-                leftMotors.setTargetVelocity(-1);
-                rightMotors.setTargetVelocity(1);
-
-                //Wait for 2 second
-                Thread.sleep(615);
-                
-                //Stop motors
-                leftMotors.setTargetVelocity(0);
-                rightMotors.setTargetVelocity(0);
-                
-                //Wait for 2 second
-                Thread.sleep(2000);
-                
-                
-                
-               
-                
-                
-                
-                
-                //2nd TURN
-                
-                
-                
-                
-                //Move forward at full speed
-                leftMotors.setTargetVelocity(1);
-                rightMotors.setTargetVelocity(1);
-
-                //Wait for 1 second
-                Thread.sleep(1750);
-                
-                //Stop motors
-                leftMotors.setTargetVelocity(0);
-                rightMotors.setTargetVelocity(0);
-                
-                //Wait for 2 second
-                Thread.sleep(2000);
-                
-                //Turn in one direction
-                leftMotors.setTargetVelocity(-1);
-                rightMotors.setTargetVelocity(1);
-
-                //Wait for 2 second
-                Thread.sleep(620);
-                
-                //Stop motors
-                leftMotors.setTargetVelocity(0);
-                rightMotors.setTargetVelocity(0);
-                
-                //Wait for 2 second
-                Thread.sleep(2000);
-                
-                
-                
-                
-                
-                
-                //
-                
-                //Move forward at full speed
-                leftMotors.setTargetVelocity(1);
-                rightMotors.setTargetVelocity(1);
-
-                //Wait for 1 second
-                Thread.sleep(1700);
-                
-                //Stop motors
-                leftMotors.setTargetVelocity(0);
-                rightMotors.setTargetVelocity(0);
-                
-                //Wait for 2 second
-                Thread.sleep(2000);
-                
-                //Turn in one direction
-                leftMotors.setTargetVelocity(-1);
-                rightMotors.setTargetVelocity(1);
-
-                //Wait for 2 second
-                Thread.sleep(628);
-                
-                //Stop motors
-                leftMotors.setTargetVelocity(0);
-                rightMotors.setTargetVelocity(0); 
-                
-                
-                //Wait for 2 second
-                Thread.sleep(2000);
-                
-                
-                //Move forward at full speed
-                leftMotors.setTargetVelocity(1);
-                rightMotors.setTargetVelocity(1);
-
-                //Wait for 1 second
-                Thread.sleep(1959);
-                
-                //Stop motors
-                leftMotors.setTargetVelocity(0);
-                rightMotors.setTargetVelocity(0);
             	  
             	
             }
@@ -255,4 +112,4 @@ public class RoverChallenge2
 }
 
 	}
-}
+
